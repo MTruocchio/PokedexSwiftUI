35f882f2e763f0c9d8e288881d746a4a1d9f5ef3
Marcelino <santanatruocchio@gmail.com>
2021-03-20 15:01:58 -0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Marcelino Santana Truocchio on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
